980eebd90a0ba950df4834caab76ffdbeae5b415
Erik Hasse <erik.g.hasse@gmail.com>
2021-01-29 20:30:10 -0500

    Add base functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import wheel from './wheel.svg';
+import ReactPlayer from 'react-player/file'
 
-function App() {
+async function getMap({timestamp}) {
+  const url = (
+    `http://192.168.0.110:8000/map/${timestamp}`
+  )
+  const response = await fetch(url)
+  const coords = await response.json()
+  return coords.longitude.map((e, i) => {
+    return `${e},${coords.latitude[i]}`
+  }).join(" ")
+}
+
+function App({timestamp}) {
+  const [telemetry, setTelemetry] = useState({});
+  const getMetrics = async (progress) => {
+    const url = (
+      `http://192.168.0.110:8000/telemetry/${timestamp}/`
+      + `${Math.floor(progress.playedSeconds * 1000)}`
+    )
+    const response = await fetch(url)
+    const telemetry = await response.json()
+    setTelemetry(telemetry)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        AX Viewer<br/>
+        <a href='static/plot-2021-01-17-11_15_03.png' target='_blank'>
+          Fit plot
         </a>
       </header>
+      <ReactPlayer
+        url='static/laps-2021-01-17-11_15_03.mp4'
+        controls={true}
+        playsinline={true}
+        progressInterval={50}
+        onProgress={getMetrics}
+      /><br/>
+    {telemetry["Speed (MPH)"]} MPH <></>
+    <img
+      src={wheel}
+      width="40"
+      style={{transform: `rotate(${telemetry["Steering Angle (deg)"]}deg)`}}
+      alt="Steering Angle"
+    /><br/>
+    <label for="throttle">Throttle: </label><progress
+        id="throttle"
+        value={telemetry["Throttle Position (%)"]}
+        max="100">
+      telemetry["Throttle Position (%)"]%
+    </progress>
+    <label for="brake">Brake: </label><progress
+        id="brake"
+        value={telemetry["Brake Pressure (bar)"]}
+        min="1"
+        max="20">
+      telemetry["Brake Pressure (bar)"]%
+    </progress><br/><br/>
     </div>
   );
 }
+    // <svg>
+    //   <polyline
+    //       points={await getMap(timestamp)}
+    //       stroke="red"
+    //       strokeWidth="3"
+    //       fill="none" />
+    // </svg>
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App timestamp="2021-01-17-11_15_03" />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
